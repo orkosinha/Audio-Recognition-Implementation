6ed241db3d06aa72cc368bc21c8f59ba8071ab0f
Samik Shrotriya <ss2659@cornell.edu>
2018-11-16 21:49:15 -0500

    Added preprocessImage to try to use VGG16

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -5,3 +5,13 @@ function readForm() {
   contentURL = form.elements[0].value;
   styleURL = form.elements[1].value;
 }
+
+function preprocessImage(image){
+  let tensor=tf.fromPixels(image)
+    .resizeNearestNeighbor([224,224])
+    .toFloat();
+  let meanImageNetRGB= tf.tensor1d([123.68,116.779,103.939]);
+  return tensor.sub(meanImageNetRGB)
+    .reverse(2)
+    .expandDims();
+}
